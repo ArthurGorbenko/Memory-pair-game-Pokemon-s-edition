04b74a1f56fbb3f2fc4dbb9be63e2eda8825fe0d
Arthur <gorbenkoartur.ur@gmail.com>
2019-06-19 15:46:17 +0300

    initital commit

---
+++ js/script.js
@@ -0,0 +1,100 @@
+const valuesOfBackPos = [
+  "20px 0px",
+  "20px -180px",
+  "20px -350px",
+  "20px -525px",
+  "-190px 0px",
+  "-390px 0px",
+  "-600px 0px",
+  "-810px 0px"
+];
+
+let cards = document.getElementsByClassName("card");
+const container = document.querySelector(".container");
+
+for (let i = 0; i < cards.length; i++) {
+  addElements(cards[i], i);
+}
+
+function addElements(element, i) {
+  let flipper = document.createElement("div");
+  flipper.className = "flipper";
+  element.appendChild(flipper);
+
+  let front = document.createElement("div");
+  front.className = "front";
+  flipper.appendChild(front);
+
+  let back = document.createElement("div");
+  back.className = "back";
+  back.dataset.numberOfElement = i;
+  flipper.appendChild(back);
+}
+
+let backs = document.querySelectorAll(".back");
+
+generateBackGrn(backs, valuesOfBackPos);
+
+function generateBackGrn(elements, arrayOfBackGrnds) {
+  let arrayOfElements = Array.from(elements);
+  arrayOfElements.sort(function() {
+    return 0.5 - Math.random();
+  });
+  arrayOfElements.forEach((element, index) => {
+    if (index >= arrayOfBackGrnds.length) {
+      element.style.backgroundPosition =
+        arrayOfBackGrnds[index - arrayOfBackGrnds.length];
+    } else {
+      element.style.backgroundPosition = arrayOfBackGrnds[index];
+    }
+  });
+}
+
+container.addEventListener("click", flip);
+
+let arrayOfFlipCards = [];
+
+function flip(event) {
+  let target = event.target;
+  while (target != container) {
+    toggleCard(target);
+    if (target.className === "flipper flipped") {
+      arrayOfFlipCards.push(target);
+    }
+    target = target.parentNode;
+  }
+  console.log(arrayOfFlipCards);
+  if (arrayOfFlipCards.length === 2) {
+    checkIfSamePicture(arrayOfFlipCards);
+    arrayOfFlipCards = [];
+  }
+}
+
+function toggleCard(element) {
+  element.classList.toggle("flipped");
+}
+
+function checkIfSamePicture(flippedCards) {
+  const firstOpenedCard = 0;
+  const secondOpenedCard = 1;
+  if (
+    flippedCards[firstOpenedCard].lastChild.style.cssText ===
+    flippedCards[secondOpenedCard].lastChild.style.cssText
+  ) {
+    if (
+      flippedCards[firstOpenedCard].lastChild.dataset.numberOfElement !==
+      flippedCards[secondOpenedCard].lastChild.dataset.numberOfElement
+    ) {
+      setTimeout(()=>{doIfFindSame(flippedCards)},1000);
+    }
+  } else {
+    setTimeout(()=> {togleBoth(flippedCards)},1000);
+  }
+}
+
+function doIfFindSame(arrayOfCards) {
+  arrayOfCards.forEach(el => el.remove());
+}
+function togleBoth(arr){
+  arr.forEach(el => toggleCard(el));
+}
